1fd1281fc93fcd04edc960728dd2aed34cc7e665
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-01-16 12:41:40 +0530

    pixel-art

---
+++ src/questions/great-frontend/Pixel Art/Canvas.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+import Cell from './Cell';
+import { Color, Mode } from './colors';
+
+
+export default function Canvas({
+  selectedColor,
+  mode,
+  initialRows = 15,
+  initialColumns = 15,
+}) {
+  const [grid, setGrid] = useState<(Color | null)[][]>(
+    Array.from({ length: initialRows }, () =>
+      Array(initialColumns).fill(null),
+    ),
+  );
+  const [isDragging, setIsDragging] = useState(false);
+
+  return (
+    <div
+      className="grid"
+      onMouseDown={() => setIsDragging(true)}
+      onMouseUp={() => setIsDragging(false)}>
+      {grid.map((row, rowIndex) => (
+        <div
+          className={[
+            'grid__row',
+            rowIndex % 2 === 0
+              ? 'grid__row--even'
+              : 'grid__row--odd',
+          ].join(' ')}
+          key={rowIndex}>
+          {row.map((cellColor, cellIndex) => (
+            <Cell
+              key={cellIndex}
+              color={cellColor}
+              isDragging={isDragging}
+              onMark={() => {
+                const newGrid = grid.map((row) => [...row]);
+                newGrid[rowIndex][cellIndex] =
+                  mode === 'erase' ? null : selectedColor;
+                setGrid(newGrid);
+              }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
